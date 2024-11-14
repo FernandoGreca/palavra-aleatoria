120b0fe18e9051a986897b912df173b0fdf42b08
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-11-14 11:29:07 -0300

    1.0.0 - Criado projeto SpringBoot

---
+++ backend/palavra/src/main/java/com/fernandogreca/palavra/PalavraApplication.java
@@ -0,0 +1,13 @@
+package com.fernandogreca.palavra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalavraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalavraApplication.class, args);
+	}
+
+}
---
+++ backend/palavra/src/test/java/com/fernandogreca/palavra/PalavraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandogreca.palavra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalavraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
