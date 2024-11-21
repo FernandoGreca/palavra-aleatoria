82345c980a7bfad342477cc38daa5a604ffc19fb
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-11-21 15:15:18 -0300

    2.1.0 - Ajustes no estilo

--- frontend/palavra-aleatoria/src/components/app/App.jsx
+++ frontend/palavra-aleatoria/src/components/app/App.jsx
@@ -27,24 +27,27 @@ function App() {
       .catch(error => console.error('Erro ao buscar palavra aleatória:', error));
   }, []);
 
-  function verificarResposta() {
+  function verificarResposta(e) {
+    e.preventDefault();
+
     var palavraApi = palavra.palavra.toLowerCase();
+    var palavraDescricao = palavra.descricao;
     var palavraResposta = resposta.toLowerCase();
+
+    if (palavraResposta.length < 1) {
+      return;
+    }
 
     setListaTentativa([...listaTentativa, palavraResposta]);
 
     if (palavraApi === palavraResposta) {
-      alert('Parabéns!! Você acertou a palavra secreta, a palavra era "' + palavraApi + '".');
-      var jogar = confirm('Deseja jogar novamente?');
-
-      if (jogar) {
-        location.reload();
-      }
-      
-      return;
-    } else {
-      setResposta('')
+      alert('Parabéns!! Você acertou a palavra secreta!!');
+      alert('Palavra: ' + palavraApi + ' Descricao: ' + palavraDescricao);
+      location.reload();
     }
+
+    setResposta('');
+    return;
   }
 
 
@@ -52,12 +55,12 @@ function App() {
     <>
       <div className='container d-flex flex-column align-items-center'>
         <div className='conteudo'>
-          <h1>Palavra Aleatória</h1>
+          <h1 className='text-light'>Palavra Aleatória</h1>
           {palavra && (
             <div className='d-flex align-items-center gap-3 mb-3'>
-              <p className='tamanhoPalavra'>Tamanho da palavra: {palavra.palavra.length}</p>
+              <p className='tamanhoPalavra text-light'>Tamanho da palavra: {palavra.palavra.length}</p>
               {/* Botão Modal */}
-              <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <FaRegLightbulb />
               </button>
 
@@ -70,7 +73,7 @@ function App() {
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                      <p><strong>{palavra.dica}</strong></p>
+                      <p><strong>{palavra.dica}.</strong></p>
                     </div>
                   </div>
                 </div>
@@ -78,25 +81,28 @@ function App() {
             </div>
           )}
 
-          <div className='row'>
-            <div className='col-xl-4 d-flex gap-2 campo-resposta'>
-              <input
-                type='text'
-                className='form-control mb-2'
-                value={resposta}
-                onChange={(e) => setResposta(e.target.value)}
-                maxLength={palavra.palavra.length}
-                placeholder='Coloque seu chute aqui...' />
+          <div className='row mb-3'>
+            <div className='col-xl-4 campo-resposta'>
+              <form className='d-flex gap-2'>
+                <input
+                  type='text'
+                  className='form-control mb-2'
+                  value={resposta}
+                  onChange={(e) => setResposta(e.target.value)}
+                  maxLength={palavra.palavra.length}
+                  placeholder='Coloque seu chute aqui...' />
 
-              <button className='btn btn-outline-success' onClick={verificarResposta}>Tentar</button>
+                <button className='btn btn-success' onClick={verificarResposta}>Tentar</button>
+              </form>
             </div>
           </div>
 
-          <div>
-            {listaTentativa.map((item, index) => (
-              <li key={index}>{item}</li>
+          <div className="lista-container">
+            {listaTentativa.slice().reverse().map((item, index) => (
+              <li key={index} className="text-light">{item}</li>
             ))}
           </div>
+
         </div>
       </div>
 
